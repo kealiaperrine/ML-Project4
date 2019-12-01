b857d9ea8fa260c24b7a89f18af40ddab8aab1b4
Josh Hosea <jphosea4@gmail.com>
2019-11-30 20:27:42 -0800

    Finished pca.py

--- pca.py
+++ pca.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 def compute_Z(X, centering=True, scaling=False):
-    if centering:
-        mean = np.mean(X, axis=0)
-        Z = np.array(X-mean)
-    if scaling:
-        standard_dev = np.std(X, axis = 0)
-        Z = np.array(X)
-        Z = Z.astype('float')
-        for i in range(X.shape[1]):
-            Z[:, i] = np.true_divide(X[:,i], standard_dev[i])
-    return Z
+
+	Z = X
+
+	if centering:
+		mean = np.mean(Z, axis = 0)
+		Z = X-mean
+
+	if scaling:
+		standard_dev = np.std(Z, axis = 0)
+		Z = Z/standard_dev
+
+	return Z
 
 def compute_covariance_matrix(Z):
     Z_t = np.transpose(Z)
@@ -26,28 +28,15 @@ def find_pcs(COV):
 
 # assume k and var are never zero at same time
 def project_data(Z, PCS, L, k, var):
-    variance = 0
-    if k == 0:
-        variance = var
-    else:
-        variance = k
 
-    eigenvectors = []
-    eigenvalues = []
-    for i in range(variance):
-        eigenvectors.append(PCS[:,i])
-        eigenvalues.append(L[i])
-    Z_star =
+	variance = 0
 
-def main():
-    # X = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
-    # X = np.array([[0, 1], [-2, -3]])
-    X = np.array([[1, 2], [ 2, 6], [3, 4], [5, 7]])
-    Z = compute_Z(X, False, True)
-    cov = compute_covariance_matrix(Z)
-    PCS, L = find_pcs(cov)
-    project_data(Z, PCS, L, 1, 0)
+	if k == 0:
 
+		while (variance < var):
+			k += 1
+			variance = sum(L[0:k])/sum(L)
 
-if __name__ == "__main__":
-    main()
+	Z_star = np.dot(Z, PCS[:,0:k])
+
+	return Z_star
